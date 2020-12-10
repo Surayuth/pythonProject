c48f225b003e0a69090f925b5a90b689bdd100d4
Surayuth <66277085+Surayuth@users.noreply.github.com>
2020-12-11 04:34:48 +0700

    Update mongodb-spark-connector.py

--- mongodb-spark-connector.py
+++ mongodb-spark-connector.py
@@ -1,16 +1,17 @@
 import datetime
 from pyspark.sql import Window as window
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
+from pyspark.sql import functions as F
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-
-
 
 # load mongo data
 input_uri = "mongodb://34.80.90.86/trendtrue_db.twitter_search"
 output_uri = "mongodb://34.80.90.86/trendtrue_db.twitter_search"
 
+# create spark session
 my_spark = SparkSession\
     .builder\
     .appName("SpaceBar")\
@@ -19,17 +20,14 @@ my_spark = SparkSession\
     .config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector_2.12:2.4.2')\
     .getOrCreate()
 
+# read data from mongodb
 df = my_spark.read.format('com.mongodb.spark.sql.DefaultSource').load()
-
-
-
 df2 = df.select('timestamp_ms', 'source')
+
 #convert timestamp
-from pyspark.sql import functions as F
 df2 = df2.withColumn("time", F.to_utc_timestamp(F.from_unixtime(F.col("timestamp_ms")/1000,'yyyy-MM-dd HH:mm:ss'),'UTC'))
 
-
-from pyspark.sql.functions import col, when
+#select tool from source
 df2 = df2.withColumn("tool", when(col("source").contains("iphone"),"iphone")
                                  .when(col("source").contains("mobile"),"mobile")
 				 .when(col("source").contains("android"),"android")
@@ -37,7 +35,6 @@ df2 = df2.withColumn("tool", when(col("source").contains("iphone"),"iphone")
 				 .when(col("source").contains("Android"),"android")
 				 .when(col("source").contains("iPad"),"iPad")
                                  .otherwise("Unknown"))
-
 df3 = df2.select('time', 'timestamp_ms',  'tool')
 df3.show()
 
